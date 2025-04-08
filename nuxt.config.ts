8c5231fd3091ac9d32672a240a0b300299d5b578
Hank <hankthl@menai.com.tw>
2025-04-09 00:25:20 +0800

    fix tailwind problem

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['@/assets/css/global.css'],
+  css: [
+    '@/assets/css/global.css',
+    '@/assets/css/main.css'
+  ],
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
